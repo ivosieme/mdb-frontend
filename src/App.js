20c396281ba304f01876dd04aa42a49332e1388b
Ivaylo Sieme <is@niv-ventures.com>
2024-04-02 18:01:43 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import StockDataTable from './StockDataTable';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Stocks Frontend App</h1>
       </header>
+      <main>
+        <h2>Stock Data refreshing every 5 sec. Server calculations done every 30 min.</h2>
+        <StockDataTable />
+      </main>
     </div>
   );
 }
---
+++ src/StockDataTable.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+
+const StockDataTable = () => {
+    const [stocks, setStocks] = useState([]);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://161.97.165.65:30869/api/stocks');
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = await response.json();
+            setStocks(data);
+        } catch (error) {
+            console.error("Could not fetch stocks: ", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+        const interval = setInterval(fetchData, 5000);
+
+        return () => clearInterval(interval);  // Clear the interval when the component unmounts
+    }, []);
+
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>Symbol</th>
+                <th>Name</th>
+                <th>Last Sale</th>
+                <th>High Sale</th>
+                <th>Low Sale</th>
+            </tr>
+            </thead>
+            <tbody>
+            {stocks.map((stock, index) => (
+                <tr key={index}>
+                    <td>{stock.symbol}</td>
+                    <td>{stock.name}</td>
+                    <td>{stock.lastSale}</td>
+                    <td>{stock.highSale}</td>
+                    <td>{stock.lowSale}</td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default StockDataTable;
