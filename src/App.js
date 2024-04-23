3c4fcd6b61b16b2e998fe8de7c1802af37c491d7
Ivaylo Sieme <is@niv-ventures.com>
2024-04-23 08:10:34 +0200

    remove title

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <h1>Stocks Frontend App</h1>
       </header>
       <main>
-        <h2>Stock Data refreshing every 5 sec. Server calculations done every 30 min.</h2>
+        <h2>Stock Data refreshing every 5 sec.</h2>
         <StockDataTable />
       </main>
     </div>
